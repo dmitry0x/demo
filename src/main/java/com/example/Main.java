32d6a0ce01859544db71840bcb689fb8c53009bf
dmitry0x <mymail.dmitry@gmail.com>
2016-10-15 13:24:19 +0300

    test commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Created by Oksana on 15.10.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("WORK");
+    }
+}
